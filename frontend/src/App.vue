fa67ec0b20544d0495bd2f8274bf61d04130f299
nochamad rifky rifaldy <mochamadrifkyrifaldy@gmail.com>
2025-04-25 09:23:16 +0700

    instal vue + tailwind

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+</template>
+
+<style scoped></style>
